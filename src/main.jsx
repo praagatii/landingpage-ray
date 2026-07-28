5cd97de12a980fdb0b72c422377d88f3ffb7ad4e
praagatii <praagatii@users.noreply.github.com>
2026-07-28 15:29:35 +0530

    add loading screen with logo

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const loader = document.getElementById('loader')
+if (loader) loader.classList.add('loaded')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
